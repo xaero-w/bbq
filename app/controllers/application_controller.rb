81649865c57a64bd31f4a45f5f40d05dd98309c7
xaero-w <d_shalimov@mail.ru>
2020-09-13 19:08:32 +0300

    add gem devise and did migration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+
+  before_action :configure_permit_parameters, if: :devise_controller?
+
+  def configure_permit_parameters
+    devise_parameter_sanitizer.for(:account_update) { |u|
+      u.permit(:password, :password_confirmation, :current-password)
+    }
+  end
 end
