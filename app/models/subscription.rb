7f1c630f13cd34dba473015cb938900b7ce24c62
xaero-w <d_shalimov@mail.ru>
2020-10-04 22:19:41 +0300

    add validates and description in model subscriptions

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -6,20 +6,18 @@ class Subscription < ActiveRecord::Base
   belongs_to :event
   belongs_to :user, optional: true
 
-  validates :event, presence: true
-
+  validates :user_owner
+  validates :existing_user, unless: -> { user.present? } 
+  
   # проверки выполняются только если user не задан (незареганные приглашенные)
   validates :user_name, presence: true, unless: -> { user.present? }
   validates :user_email, presence: true, format: /\A[a-zA-Z0-9\-_.]+@[a-zA-Z0-9\-_.]+\z/, unless: -> { user.present? }
 
   # для данного event_id один юзер может подписаться только один раз (если юзер задан)
-  validates :user, uniqueness: {scope: :event_id}, if: -> { user.present? }
+  validates :user, uniqueness: { scope: :event_id }, if: -> { user.present? }
 
-  # для данного event_id один email может использоваться только один раз (если нет юзера, анонимная подписка)
-  validates :user_email, uniqueness: {scope: :event_id}, unless: -> { user.present? }
-
-  # запрет пользователю поджписываться на собственные события
-  validate :email_taken, unless: -> { user.present? }
+  # или один email может использоваться только один раз (если анонимная подписка)
+  validates :user_email, uniqueness: { scope:  :event_id }, unless: -> { user.present? }
 
   # переопределяем метод, если есть юзер, выдаем его имя,
   # если нет -- дергаем исходный переопределенный метод
