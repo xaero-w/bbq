9356d6f0035f19fbb610218ddb2241e0ab8ba0ed
xaero-w <d_shalimov@mail.ru>
2020-11-16 20:31:18 +0300

    added the ability to recover password and send mail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,7 +32,7 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
@@ -59,5 +59,20 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # Базовый урл сайта, для генерации правильных ссылок в письмах
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
+  # отправка почты по протоколу SMTP
+  config.action_mailer.delivery_method = :smtp
+
+  # Настройки для работы через GMail аккаунт
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: '587',
+    user_name: 'testrailsw', # не используйте для тестов свои реальные ящики
+    password: 'acwwps!1', # не храните здесь пароль!
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
 end
