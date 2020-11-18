b56bc54ce1d4eaa47e100a192350d069a6a05dd2
xaero-w <d_shalimov@mail.ru>
2020-11-18 18:12:38 +0300

    added sendgrid and event-mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -70,8 +70,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: '587',
-    user_name: 'testrailsw', # не используйте для тестов свои реальные ящики
-    password: 'acwwps!1', # не храните здесь пароль!
+    user_name: '#{}', # не используйте для тестов свои реальные ящики
+    password: '', # не храните здесь пароль!
     authentication: 'plain',
     enable_starttls_auto: true
   }
