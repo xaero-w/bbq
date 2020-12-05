365e6790f72a68b66142a86362813bd0479542c9
xaero-w <d_shalimov@mail.ru>
2020-12-05 22:42:25 +0300

    added a pin code, everything works (no homework)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_07_175821) do
+ActiveRecord::Schema.define(version: 2020_12_03_150930) do
 
   create_table "comments", force: :cascade do |t|
     t.text "body"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 2020_10_07_175821) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id", null: false
+    t.string "pincode"
     t.index ["user_id"], name: "index_events_on_user_id"
   end
 
