5c61ad90da8864dfa8e97a0de80fc865d8dafd28
xaero-w <d_shalimov@mail.ru>
2020-12-20 17:52:46 +0300

    added yandex map

---
+++ app/javascript/packs/map.js
@@ -0,0 +1,31 @@
+ymaps.ready(init);
+var myMap;
+
+function init(){
+    address = document.getElementById('map').getAttribute('data-address');
+
+    myMap = new ymaps.Map("map", {
+        center: [55.76, 37.64],
+        zoom: 10
+    });
+
+    myGeocoder = ymaps.geocode(address);
+
+    myGeocoder.then(
+        function (res) {
+            coordinates = res.geoObjects.get(0).geometry.getCoordinates();
+
+            myMap.geoObjects.add(
+                new ymaps.Placemark(
+                    coordinates,
+                    {preset: 'islands#blueStretchyIcon'}
+                )
+            );
+
+            myMap.setCenter(coordinates);
+            myMap.setZoom(15);
+        }, function (err) {
+            alert('Произошла ошибка. Попробуйте еще раз');
+        }
+    );
+}
